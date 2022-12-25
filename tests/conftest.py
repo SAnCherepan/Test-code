d019aa379cf6202d5d644e619d2cd1dd262b61d2
SAnCherepan <SAnCherepan@users.norely.github.com.com>
2022-12-25 11:29:04 +0700

    Added tests

---
+++ tests/conftest.py
@@ -0,0 +1,33 @@
+import pytest
+import json
+
+github_base_url = "https://api.github.com"
+
+
+# ToDo: recursively lookup resources directory and generate mocks wherever response.json is present
+@pytest.fixture(autouse=True)
+def request_register_get_urls(requests_mock):
+    # list of repositories
+    with open('tests/resources/orgs/docker/repos/response.json', 'r') as fp:
+        requests_mock.get(
+            f'{github_base_url}/orgs/docker/repos',
+            json=json.loads(fp.read())
+        )
+
+    # branches for individual repositories
+    repos = ['docker-py', 'compose', 'docs']
+    for repo in repos:
+        with open(f'tests/resources/repos/docker/{repo}/branches/response.json', 'r') as fp:
+            requests_mock.get(
+                f'{github_base_url}/repos/docker/{repo}/branches',
+                json=json.loads(fp.read())
+            )
+
+    return requests_mock
+
+# @pytest.fixture(autouse=True)
+# def requests_mock_all(requests_mock):
+#     """
+#     Mock all HTTP requests outside - only use mocks
+#     Works for 'requests' library only
+#     """
